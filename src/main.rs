12fe263eab38b7a0e7d7ee232bd79e30d0f95a3c
reacktivo <65733222+reacktivo@users.noreply.github.com>
2020-05-22 18:55:36 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,7 @@ use iron::status;
 
 fn main() {
     fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
-        println!("Universidad Politecnica de San Luis potosi");
-        println!("Isaac Amalio Perez Macareno 180672");
-        println!("Chávez Flores Rebeca 171176");
-        println!("Cruz Becerra Rodrigo Emmanuel 170736");
+        Ok(Response::with((status::Ok, "Universidad Politecnica de San Luis potosi. Isaac Amalio Perez Macareno 180672, Chávez Flores Rebeca 171176, Cruz Becerra Rodrigo Emmanuel 170736")))
     }
 
     let _server = Iron::new(hello_world).http("0.0.0.0:8080").unwrap();
